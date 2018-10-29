aa5a11eaebc14aeef9127cc88e7b6982dfbfd4cf
LongQiudao <2224022091@qq.com>
2018-10-29 21:32:21 +0800

    fristcommit

---
+++ mybatisReverseEngineering/src/ssm/po/TbUser.java
@@ -0,0 +1,63 @@
+package ssm.po;
+
+public class TbUser {
+    private String uid;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String code;
+
+    private Boolean state;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid == null ? null : uid.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+}
